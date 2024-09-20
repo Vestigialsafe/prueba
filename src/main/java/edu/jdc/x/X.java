5a0a8d3e8266812e8561104be3ead0d24cb7e7ff
carva <carva@PC-AFCP>
2024-09-20 09:45:16 -0500

    carga inicial

---
+++ src/main/java/edu/jdc/x/X.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.jdc.x;
+
+/**
+ *
+ * @author carva
+ */
+public class X {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
