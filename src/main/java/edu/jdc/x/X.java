6d7a02918ffbc0335642ae55dcccb0478a1531a1
Vestigialsafe <carvajalfelipe383@gmail.com>
2024-09-20 10:11:25 -0500

    Update X.java

--- src/main/java/edu/jdc/x/X.java
+++ src/main/java/edu/jdc/x/X.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package edu.jdc.x;
 
-/**
- *
- * @author carva
- */
 public class X {
 
     public static void main(String[] args) {
